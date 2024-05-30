c5587f4ca2a0a784cb3cbc1cfbe6ddb95dd8a3f7
AASHIN A K <aashin9605@gmail.com>
2024-05-30 22:16:11 +0530

    day 3

---
+++ 03_operations.js
@@ -0,0 +1,42 @@
+//**************************************operations**************************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);  // -3
+
+// console.log(2**4); // 16
+
+// console.log("2" + 2 ); // 22
+// console.log("2" + 2 + 2); // 222
+// console.log(2 + 2 + "2"); // 42
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+
+// let a = 5
+// ++a
+// console.log(a); // 6   
+
+// let a = 5
+// a++
+// console.log(a); // 6  // what happens here while a++ it returns 5 then increments to 6 , so when doing console.log it shows 6
+
+// let a = 5
+// console.log(a++); // 5  // it directly returns 5 so it prints 5 then increment to 6
+
+// let a = 5
+// console.log(++a); // 6
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@ COMPARISON @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+console.log("2" > 1); // true
+
+console.log(null == 0); // false
+console.log(null > 0); // false
+console.log(null >= 0); // true
+// comparisons convert null to a number , treating it as 0
+// working of == and comparisons is different
+
+
+
