2b79489142d88643abdb951a683d6abe23f93917
AASHIN A K <aashin9605@gmail.com>
2024-05-31 23:07:41 +0530

    day 4

---
+++ 04_datatype_summary.js
@@ -0,0 +1,23 @@
+// primitive (call by value)
+
+// string,number,boolean,null,undefined,symbol,bigint
+
+
+// reference type (non primitive)
+// master objects and browser events
+// arrays, objects, functions
+
+// symbol makes it unique
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId, typeof id); // false symbol
+
+// const bigNumber = 434858883685786869853634n
+// console.log(typeof bigNumber); // bigint
+
+//typeof array => object
+//typeof object => object
+//typeof function => function  (function object)
+
+
+
