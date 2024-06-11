b7037159cbd7ffdc1225f37dc9e2e95183f2a07b
Aashin A K <aashin9605@gmail.com>
2024-06-11 11:04:51 +0530

    day 10

---
+++ 10_destructure_object_and_functions.js
@@ -0,0 +1,23 @@
+// const details = {
+//     firstName: "Aashin",
+//     email: "aash@gmail.com",
+//     place: "Kerala"
+// }
+
+// const {firstName, place, email} = details
+// console.log(firstName, place, email); // Aashin Kerala aash@gmail.com
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@ FUNCTIONS @@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// function printMyName(name) {
+//     return `hey ${name} you just logged in`
+// }
+
+// console.log(printMyName("Aashin"));// hey Aashin you just logged in
+// console.log(printMyName()); // hey undefined you just logged in
+
+function printMyNamee(name) {
+    return !name? "enter name" : name
+    
+}
+console.log(printMyNamee("Aashin"));
