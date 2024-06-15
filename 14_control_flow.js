4c277efc783d40a0908fc23fe871e71a4c43b7e6
Aashin A K <aashin9605@gmail.com>
2024-06-15 07:25:00 +0530

    day 14

---
+++ 14_control_flow.js
@@ -0,0 +1,12 @@
+// refer if else
+// truthy and falsy
+// Nullish coalescing operator ??
+let val1
+val1 = 5 ?? 10 // 5
+val1 = null ?? 20 // 20
+val1 = undefined ?? 10 // 10
+val1 = null ?? 10 ?? 20 // 10
+console.log(val1);
+
+// ternary operators
+// loops
